a1cf191cb2b602b2af4bbc77c7c7720c90f4c82b
crbalol34 <cristobalveracaceres@gmail.com>
2025-11-17 15:53:22 -0300

    Update ejemplo_titanic (1).py

--- ejemplo_titanic (1).py
+++ ejemplo_titanic (1).py
@@ -24,7 +24,7 @@ with st.sidebar:
     
     # Slider de Bins (Lo que ya tenías)
     div = st.slider('Número de bins:', 0, 20, 10)
-    fig, ax = plt.subplots(figsize=(12, 4))
+    fig, ax = plt.subplots(figsize=(5, 3))
     
     st.write("---")
     
